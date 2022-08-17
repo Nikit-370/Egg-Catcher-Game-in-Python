dacc2f49d309b08718b522d28811002a31f38826
Nikit Singh Kanyal <81354106+Nikit-370@users.noreply.github.com>
2022-08-17 17:59:24 +0530

    Update Egg Catcher.py

--- Egg Catcher.py
+++ Egg Catcher.py
@@ -2,10 +2,11 @@ from itertools import cycle
 from random import randrange
 from tkinter import Tk , Canvas , messagebox , font
 
-canvas_width = 800
-canvas_height = 400
+canvas_width = 1200
+canvas_height = 800
 
 win = Tk()
+win.title('Egg Catcher')
 c = Canvas(win , width = canvas_width ,  height = canvas_height , background = 'deep sky blue')
 c.create_rectangle(-5, canvas_height - 100 , canvas_width + 5 , canvas_height + 5 , fill='sea green', width=0)
 c.create_oval(-80,-80,120,120,fill='orange' , width=0)
